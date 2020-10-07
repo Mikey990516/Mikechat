86c534cf0025e7a9221ec21d5f2f346713b0b846
Mikey990516 <70058534+Mikey990516@users.noreply.github.com>
2020-10-08 00:39:51 +0200

    Update Mikechat.php

--- Mikechat.php
+++ Mikechat.php
@@ -1,10 +1,36 @@
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Mikechat</title>
+	
 </head>
 <body>
-	<h1> MikeChat </h1>
-	<p>You must accept at your emails</p>
+
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	
+	<form method="post" action="login.html">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.html">Sign up</a>
+		</p>
+	</form>
+
+
 </body>
 </html>
