22e240d4b60405370dff63919ba338eed92b9647
Alec <alecshelembe@gmail.com>
2020-10-08 01:01:42 +0200

    Add files via upload

---
+++ alecindex.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mikechat</title>
+</head>
+<body>
+	<h1>Login</h1>
+	<form action="#" method="post">
+		<input type="text" name="email" placeholder="Email">
+		<input type="password" name="password" placeholder="Password">
+		<input type="submit" value="Login">
+	</form>
+
+	<h1>Sign Up</h1>
+
+	<form action="#" method="post">
+		<input type="text" name="username" placeholder="username">
+		<input type="text" name="emial" placeholder="email">
+		<input type="password" name="password" placeholder="Password">
+		<input type="submit" value="Create account">
+	</form>
+</body>
+</html>
