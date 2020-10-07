2f5b6298ff8bca5cc568e14c037836739d6feaeb
Mikey990516 <70058534+Mikey990516@users.noreply.github.com>
2020-10-08 01:03:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,27 @@
 	<input type="submit" value="Login">
 
 	<h1>Sign Up</h1>
+	<fieldset>
+		<form>
+			<table cellpadding="5" cellspacing="5">
+				<tr>
+					<td><label>First name*</label></td>
+					<td><label>Last name *</label></td>
+				</tr>
+				<tr>
+					<td><input type="text" name="firstname" placeholder="Enter your firstname></td>
+					<td><input type="text" name="lastname" placeholder="Enter your lastname></td>
+				</tr>
+				<tr>
+					<td><label>User name*</label></td>
+					<td><label>Repeat user name*</label></td>
+				</tr>
+				<tr>
+					<td><input type="text" name="username" placeholder="Enter your username></td>
+		
+				</tr>
+
+			</table>
+		</fieldset>
 </body>
 </html>
