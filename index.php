429fe4ac70865b9ed4cfe63173809d8203536fc6
Mikey990516 <70058534+Mikey990516@users.noreply.github.com>
2020-10-08 01:09:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,33 +5,17 @@
 </head>
 <body>
 	<h1>Login</h1>
-	<input type="text" name="username" placeholder="username">
-	<input type="password" name="password" placeholder="Password">
-	<input type="submit" value="Login">
-
+	<form action="#" method="post">
+		<input type="text" name="email" placeholder="Email">
+		<input type="password" name="password" placeholder="Password">
+		<input type="submit" value="Login">
+	</form>
 	<h1>Sign Up</h1>
-	<fieldset>
-		<form>
-			<table cellpadding="5" cellspacing="5">
-				<tr>
-					<td><label>First name*</label></td>
-					<td><label>Last name *</label></td>
-				</tr>
-				<tr>
-					<td><input type="text" name="firstname" placeholder="Enter your firstname></td>
-					<td><input type="text" name="lastname" placeholder="Enter your lastname></td>
-				</tr>
-				<tr>
-					<td><label>User name*</label></td>
-					<td><label>Repeat user name*</label></td>
-				</tr>
-				<tr>
-					<td><input type="text" name="username" placeholder="Enter your username></td>
-		
-				</tr>
-
-			</table>
-		   </form>
-		</fieldset>
+	<form action="#" method="post">
+		<input type="text" name="username" placeholder="username">
+		<input type="text" name="emial" placeholder="email">
+		<input type="password" name="password" placeholder="Password">
+		<input type="submit" value="Create account">
+	</form>
 </body>
 </html>
