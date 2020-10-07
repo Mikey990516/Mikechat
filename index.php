fdadbfe420f6de040a9da2c8de0f63db1de48dfe
Alec <alecshelembe@gmail.com>
2020-10-08 00:56:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 	<title>Mikechat</title>
 </head>
 <body>
-	<h1> Chat </h1>
-	<p>You must accept at your emails</p>
+	<h1>Login</h1>
+	<input type="text" name="username" placeholder="username">
+	<input type="password" name="password" placeholder="Password">
+	<input type="submit" value="Login">
+
+	<h1>Sign Up</h1>
 </body>
 </html>
